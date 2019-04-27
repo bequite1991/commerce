306996a4037088cff088b99ff3860b5fb8d89643
xiaobc <527710279@qq.com>
2019-04-27 13:09:08 +0800

    优化

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,23 +1,25 @@
 import Taro from '@tarojs/taro'
+
+const API_HOST = 'http://118.25.103.49:8000';
+
+console.log('API_HOST:',API_HOST);
 export default function request(url, options) {
+  if(!url.startsWith('http')){
+    url = API_HOST + url;
+  }
+  // 获取存储的sessionId
+  const cookie = wx.getStorageSync("_TY_S");
   let newOptions = { ...options };
   if (newOptions.method === 'POST' || newOptions.method === 'PUT') {
-    if (!(newOptions.body instanceof FormData)) {
-      newOptions.header = {
-        Accept: 'application/json',
-        'Content-Type': 'application/json; charset=utf-8',
-        ...newOptions.header
-      };
-    } else {
-      // newOptions.body is FormData
-      newOptions.header = {
-        Accept: 'application/json',
-        ...newOptions.header
-      };
+    newOptions.header = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      'Cookie': (cookie ? 'JSESSIONID=' + cookie : ''),
+      ...newOptions.header
     }
   } else {
     newOptions.header = {
-      'Content-Type': 'json',  // 这里是特殊处理
+      'Content-Type': 'application/json',  // 这里是特殊处理
+      'Cookie': (cookie ? 'JSESSIONID=' + cookie : ''),
       ...newOptions.header
     }
   }
